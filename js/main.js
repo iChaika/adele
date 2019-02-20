0f77704991a80b34ef5277bcb2db0c3577a63d60
Den <zz.smile@yandex.com>
2019-02-20 22:29:03 +0300

    добавлен no-scroll для меню

--- js/main.js
+++ js/main.js
@@ -4,11 +4,13 @@ let menuBlock = document.querySelector('.main-menu');
 menuBtn.addEventListener('click', function() {
     menuBtn.classList.toggle('active-menu');
     menuBlock.classList.toggle('menu-open');
+    document.body.classList.toggle('no-scroll');
 
     let escToggle = function(e) {
         if(e.keyCode == 27) {
             menuBlock.classList.toggle('menu-open');
             menuBtn.classList.toggle('active-menu');
+            document.body.classList.toggle('no-scroll');
             document.removeEventListener('keydown', escToggle, false);
         }
     }
